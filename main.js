01ed414aa9f6b4273000610d31ff52556712b2f9
Kathryn Wohl <ktwohl@gmail.com>
2020-01-21 13:48:55 -0600

    wrote functions to submit and gather orders and output order list to a table

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const restaurant = {
     name: "Bob's Burgers",
     orders: [],
     placeOrder: function (meal) {
-
+        this.orders.push(meal);
     },
     order: function () {
-        
+        return this.orders
     }
 
 }
@@ -38,6 +38,7 @@ const avocadoToast = {
 restaurant.placeOrder(avocadoToast)
 
 // Invoke the function to return the list of all orders
-
+const orderList = restaurant.order()
+console.log(orderList)
 // Output all orders to the console using console.table()
-console.table();
+console.table(restaurant.orders);
